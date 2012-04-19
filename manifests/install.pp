1a7ca92bda0205efc27dbf3888e5b8adaf21c9b2
andrejsab <andrejsab@gmail.com>
2012-04-19 15:16:27 +0200

    Update manifests/install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -19,8 +19,13 @@ class apache::install {
     name   => $apache::params::package_name,
     ensure => latest,
   }
+$str= "<html><body><h1>It works- I changed it!</h1>
+<p>This is the default web page for this server.</p>
+<p>The web server software is running but no content has been added, yet.</p>
+</body></html>
+"
 file { "/var/www/index.html":
 
-source => "/var/www/index.html",
+content => "$str",
 }
 }
